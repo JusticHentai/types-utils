bcc4aecf8c0288e10cd642591ee8252d8459a10a
JusticHentai <1360636180@qq.com>
2022-11-07 11:42:34 +0800

    feat: pick

--- src/Pick.ts
+++ src/Pick.ts
@@ -1,7 +1,7 @@
 /**
  * 从一个interface: Type中 获取指定key 组成新的interface
  */
-type Pick<Type, KeyList extends keyof Type> = {
+type Pick<Type extends Record<string, any>, KeyList extends keyof Type> = {
   [key in KeyList]: Type[key]
 }
 
